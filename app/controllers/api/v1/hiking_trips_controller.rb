625f2c4b32054d413745e07d2ae783bf4aea2b22
slee713 <steven.lee1393@gmail.com>
2020-10-08 12:35:43 -0400

    create hiking trip controller method

---
+++ app/controllers/api/v1/hiking_trips_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::HikingTripsController < ApplicationController
+    def index
+    end
+
+    def create 
+        hiking_trip = HikingTrip.new(hiking_trip_params)
+
+        if hiking_trip.valid?
+            hiking_trip.save
+            render json: {message: 'Hiking Trip Successfully Created!'} 
+        else 
+            render json: {error: hiking_trip.errors.full_messages.join(';')}       
+    end
+
+
+    def hiking_trip_params
+        params.permit(:hiking_project_id, :name, :start_date, :end_date, :description)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
+      resources :hiking_trips
       post '/login', to: 'auth#create'
     end
   end

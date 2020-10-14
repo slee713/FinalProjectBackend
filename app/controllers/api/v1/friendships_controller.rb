736c63d93c29e6edf92a09262f48c2994363dce5
slee713 <steven.lee1393@gmail.com>
2020-10-14 17:19:16 -0400

    add and remove friend

--- app/controllers/api/v1/friendships_controller.rb
+++ app/controllers/api/v1/friendships_controller.rb
@@ -6,10 +6,30 @@ class Api::V1::FriendshipsController < ApplicationController
     end
 
     def create
-        
+        friendship = Friendship.new(friendship_params)
+        if friendship.valid?
+            friendship.save
+            render json: friendship
+        else 
+            render json: {error: 'Unable To add friend'}
+        end
     end
 
     def destroy
+        friendship = Friendship.find(params[:id])
+        friendship.destroy
     end
 
+    def remove_friend
+        friendship1 = Friendship.find_by(user_id: @user.id, friend_id: params[:friend_id])
+        friendship2 = Friendship.find_by(user_id: params[:friend_id], friend_id: @user.id)
+        friendship1.destroy
+        friendship2.destroy
+    end
+
+    private 
+
+    def friendship_params
+        params.permit(:user_id, :friend_id)
+    end
 end

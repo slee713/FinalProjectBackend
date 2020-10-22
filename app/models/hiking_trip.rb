89c0d7c004c4092fa80b057f403b4c0863b79fbc
slee713 <steven.lee1393@gmail.com>
2020-10-22 12:22:52 -0400

    remove img validation

--- app/models/hiking_trip.rb
+++ app/models/hiking_trip.rb
@@ -12,6 +12,6 @@ class HikingTrip < ApplicationRecord
     validates :start_date, presence: true
     validates :end_date, presence: true
     validates :hiking_project_id, presence: true
-    validates :img, presence: true
+
 
 end
